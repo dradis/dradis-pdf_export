57999e949358dcf142663fda9b905207cea6ce72
Aaron Manaloto <mail@aapomm.io>
2023-03-28 20:07:00 +0800

    Remove validation

--- app/controllers/dradis/plugins/pdf_export/base_controller.rb
+++ app/controllers/dradis/plugins/pdf_export/base_controller.rb
@@ -2,8 +2,6 @@ module Dradis
   module Plugins
     module PdfExport
       class BaseController < Dradis::Plugins::Export::BaseController
-        before_action :validate_scope
-
         # This method cycles throw the notes in the reporting category and creates
         # a simple PDF report with them.
         def index
@@ -15,16 +13,6 @@ module Dradis
           send_data pdf.render, filename: "dradis_report-#{Time.now.to_i}.pdf",
                                 type: 'application/pdf',
                                 disposition: 'inline'
-        end
-
-        private
-
-        def validate_scope
-          @scope = params[:scope]
-
-          unless @scope == 'all' || @scope == 'published'
-            raise 'Something fishy is going on...'
-          end
         end
       end
     end
